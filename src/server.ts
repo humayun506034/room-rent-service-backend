1df4d139ce0b8197e018e0b8a990be835f96ba67
Md. Humayun Kabir Sobuj <humayun506034@gmail.com>
2026-01-25 12:29:41 +0600

    modify

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,12 @@ import { configs } from "./app/configs";
 import app from "./app";
 
 async function main() {
-  console.log(process.env.DB_URL);
+  // console.log(process.env.DB_URL);
 
-  const databaseUrl =
-    process.env.DB_URL || "mongodb://127.0.0.1:27017/yannyamba";
+  const databaseUrl = process.env.DB_URL;
   // const databaseUrl = "mongodb://127.0.0.1:27017/yannyamba";
 
-  await mongoose.connect(databaseUrl);
+  await mongoose.connect(databaseUrl as string);
   app.listen(configs.port, () => {
     console.log(`Server running on port ${configs.port}`);
   });
